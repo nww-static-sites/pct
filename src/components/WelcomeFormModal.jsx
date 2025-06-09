e87d95dab799d5271c9f6d24e30616a92107256c
shawn-nww <67067973+shawn-nww@users.noreply.github.com>
2025-06-09 07:54:14 -0400

    Update WelcomeFormModal.jsx

--- src/components/WelcomeFormModal.jsx
+++ src/components/WelcomeFormModal.jsx
@@ -6,13 +6,15 @@ import ModalData from "./Modals/WelcomeModal/ModalData";
 
 export const WelcomeFormModal = () => {
   const [isOpen, setIsOpen] = useState(false);
+
   useEffect(() => {
-    const isSubmitted = localStorage.getItem("isGuideSubmitted", "true") === "true" ? false : true;
-    setIsOpen(isSubmitted);
+    // Ensure the modal never opens
+    setIsOpen(false);
   }, [])
+
   return (
     <HomeModal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-      <ModalData onClose={() => setIsOpen(false)}/>
+      <ModalData onClose={() => setIsOpen(false)} />
     </HomeModal>
   );
 };
