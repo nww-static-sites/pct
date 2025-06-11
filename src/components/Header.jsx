6e3bc5258c8fd9c532199816755c369ce3795d2b
shawn-nww <67067973+shawn-nww@users.noreply.github.com>
2025-06-11 12:31:38 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import logo from "public/pct_logo_2x.png";
+import logo from "public/pct_logo.png";
 import { FullWidthContent } from "./Sections";
 import { useContext, useEffect, useState } from "react";
 import { DesignContext } from "@/context/design";
