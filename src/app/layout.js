964fbb8a605389e5a2e2faed029b8152d8be8c48
shawn-nww <67067973+shawn-nww@users.noreply.github.com>
2025-07-25 06:11:03 -0400

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -38,6 +38,23 @@ export default function RootLayout({ children }) {
           {children}
           <Footer />
         </DesignProvider>
+          <Script id="pardot-tracking" strategy="afterInteractive">
+  {`
+    piAId = '1109132';
+    piCId = '172544';
+    piHostname = 'go.pctconformalcoating.com';
+
+    (function() {
+      function async_load(){
+        var s = document.createElement('script'); s.type = 'text/javascript';
+        s.src = (document.location.protocol === 'https:' ? 'https://' : 'http://') + piHostname + '/pd.js';
+        var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
+      }
+      if(window.attachEvent) { window.attachEvent('onload', async_load); }
+      else { window.addEventListener('load', async_load, false); }
+    })();
+  `}
+</Script>
       </body>
     </html>
   );
